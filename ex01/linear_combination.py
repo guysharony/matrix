b98a24d654386ffe6e0760d24e1c288fbd9346e5
Guy Sharony <40563195+guysharony@users.noreply.github.com>
2023-12-11 15:19:23 +0100

    ex01

---
+++ ex01/linear_combination.py
@@ -0,0 +1,29 @@
+from classes.vector import Vector
+
+
+def linear_combination(u: list[Vector], coefs: list[float]) -> Vector:
+    """
+    Computes the linear combination of vectors.
+
+    Args:
+        u (list[Vector]): A list of Vector objects.
+        coefs (list[float]): A list of coefficients.
+
+    Returns:
+        Vector: A Vector representing the linear combination of vectors in u with the given coefficients.
+    """
+
+    if u.__class__ != list or not all(vector.__class__ == Vector for vector in u):
+        raise TypeError("u must be a list of Vector")
+
+    if coefs.__class__ != list or not all(coef.__class__ in (float, int) for coef in coefs):
+        raise TypeError("coefs must be a list of int or float")
+
+    if len(u) != len(coefs):
+        raise ValueError("u and coefs must be of the same size")
+
+    result_vector = Vector([0.0] * u[0].length)
+    for vector, coef in zip(u, coefs):
+        result_vector.add(vector.scl(coef))
+
+    return result_vector
