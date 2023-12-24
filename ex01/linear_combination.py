8cae6c644dc2eb6ae72d0b2c73a6ba0cc36b4ecf
Guy Sharony <40563195+guysharony@users.noreply.github.com>
2023-12-24 12:23:50 +0100

    Adding complexity

--- ex01/linear_combination.py
+++ ex01/linear_combination.py
@@ -5,6 +5,10 @@ def linear_combination(u: list[Vector], coefs: list[float]) -> Vector:
     """
     Computes the linear combination of vectors.
 
+    Complexity:
+        Time: O(n)
+        Space: O(n)
+
     Args:
         u (list[Vector]): A list of Vector objects.
         coefs (list[float]): A list of coefficients.
@@ -14,16 +18,16 @@ def linear_combination(u: list[Vector], coefs: list[float]) -> Vector:
     """
 
     if u.__class__ != list or not all(vector.__class__ == Vector for vector in u):
-        raise TypeError("u must be a list of Vector")
+        raise TypeError("u must be a list of Vector.")
 
     if coefs.__class__ != list or not all(coef.__class__ in (float, int) for coef in coefs):
-        raise TypeError("coefs must be a list of int or float")
+        raise TypeError("coefs must be a list of int or float.")
 
     if len(u) != len(coefs):
-        raise ValueError("u and coefs must be of the same size")
+        raise ValueError("u and coefs must be of the same size.")
 
-    result_vector = Vector([0.0] * u[0].get_size())
-    for vector, coef in zip(u, coefs):
-        result_vector.add(vector.scl(coef))
+    result_vector = Vector([0.0] * u[0].get_size()) # Space complexity of O(n)
+    for vector, coef in zip(u, coefs): # Void as "The 'n' in complexity is the dimension of the vector/matrix".
+        result_vector.add(vector.scl(coef)) # Complexity of O(n)
 
     return result_vector
--- ex02/linear_interpolation.py
+++ ex02/linear_interpolation.py
@@ -3,29 +3,45 @@ from classes.matrix import Matrix
 from classes.vector import Vector
 
 def lerp(u: Union[float, Vector, Matrix], v: Union[float, Vector, Matrix], t: float) -> Union[float, Vector, Matrix]:
-    if t < 0 or t > 1:
+    """
+    Performs linear interpolation between two values, vectors, or matrices based on a scalar value 't'.
+
+    Complexity:
+        Time: O(n)
+        Space: O(n)
+
+    Args:
+        u (Union[float, Vector, Matrix]): First value, vector, or matrix for interpolation.
+        v (Union[float, Vector, Matrix]): Second value, vector, or matrix for interpolation.
+        t (float): Scalar value between 0 and 1 determining the interpolation ratio.
+
+    Returns:
+        Union[float, Vector, Matrix]: The interpolated value, vector, or matrix based on 't'.
+    """
+
+    if t < 0 or t > 1: # Condition of complexity of O(1).
         raise ValueError("Scaler must be between 0 and 1")
 
-    if u.__class__ in (int, float) and v.__class__ in (int, float):
-        return round((1 - t) * u + t * v, 1)
+    if u.__class__ in (int, float) and v.__class__ in (int, float): # Condition of complexity of O(1).
+        return round((1 - t) * u + t * v, 1) # Complexity of O(1) (operation).
 
-    if u.__class__ == Vector and v.__class__ == Vector:
-        u_size = u.get_size()
-        v_size = v.get_size()
+    if u.__class__ == Vector and v.__class__ == Vector: # Condition of complexity of O(1).
+        u_size = u.get_size() # Space and time complexity of O(1).
+        v_size = v.get_size() # Space and time complexity of O(1).
 
-        if u_size != v_size:
+        if u_size != v_size: # Space and time complexity of O(1).
             raise ValueError("Vectors must be of the same size.")
 
         return Vector([
             round((1 - t) * a + t * b, 1)
             for a, b in zip(u, v)
-        ])
+        ]) # Loop u and v so complexity of O(n).
 
-    if u.__class__ == Matrix and v.__class__ == Matrix:
-        u_rows, u_columns = u.get_shape()
-        v_rows, v_columns = v.get_shape()
+    if u.__class__ == Matrix and v.__class__ == Matrix: # Condition of complexity of O(1).
+        u_rows, u_columns = u.get_shape() # Space and time complexity of O(1).
+        v_rows, v_columns = v.get_shape() # Space and time complexity of O(1).
 
-        if u_rows != v_rows or u_columns != v_columns:
+        if u_rows != v_rows or u_columns != v_columns: # Condition of complexity of O(1).
             raise ValueError("Matrix must be of the same size.")
 
         return Matrix([
@@ -33,6 +49,6 @@ def lerp(u: Union[float, Vector, Matrix], v: Union[float, Vector, Matrix], t: fl
                 round((1 - t) * a + t * b, 1)
                 for a, b in zip(row_u, row_v)
             ] for row_u, row_v in zip(u, v)
-        ])
+        ]) # Complexity of O(n) as "The 'n' in complexity is the dimension of the vector/matrix".
 
     return TypeError(f"Cannot compute linear interpolation of {type(u)} and {type(v)}.")
