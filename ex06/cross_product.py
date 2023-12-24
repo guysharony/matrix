3bf77338d75ffc6ccaee971fdd702d842126314f
Guy Sharony <40563195+guysharony@users.noreply.github.com>
2023-12-24 14:31:13 +0100

    Complexity ex06

--- ex06/cross_product.py
+++ ex06/cross_product.py
@@ -4,22 +4,23 @@ def cross_product(u: Vector, v: Vector) -> Vector:
     """
     Calculate the cross product of two 3-dimensional vectors.
 
+    Complexity:
+        Time: O(n)
+        Space: O(n)
+
     Args:
         u (Vector): First vector.
         v (Vector): Second vector.
-
-    Raises:
-        ValueError: If the input vectors are not 3-dimensional.
 
     Returns:
         Vector: The resulting vector from the cross product operation.
     """
 
-    if u.get_size() != 3 or v.get_size() != 3:
+    if u.get_size() != 3 or v.get_size() != 3: # Space and time complexity of O(1).
         raise ValueError("Vectors must be 3-dimensional.")
 
     return Vector([
         u.data[1] * v.data[2] - v.data[1] * u.data[2],
         u.data[2] * v.data[0] - v.data[2] * u.data[0],
         u.data[0] * v.data[1] - v.data[0] * u.data[1]
-    ])
+    ]) # Space and time complexity of O(n).
