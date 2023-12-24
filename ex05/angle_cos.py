a55b726f0c828e3c73de9ba169aacabba66a9d4f
Guy Sharony <40563195+guysharony@users.noreply.github.com>
2023-12-24 14:28:54 +0100

    Complexity ex05

--- ex05/angle_cos.py
+++ ex05/angle_cos.py
@@ -4,6 +4,10 @@ def angle_cos(u: Vector, v: Vector) -> float:
     """
     Calculate the cosine similarity between two vectors.
 
+    Complexity:
+        Time: O(n)
+        Space: O(1)
+
     Args:
         u (Vector): First vector.
         v (Vector): Second vector.
@@ -11,10 +15,10 @@ def angle_cos(u: Vector, v: Vector) -> float:
     Returns:
         float: Cosine similarity between the input vectors.
     """
-    if all(value == 0 for value in v.data) or all(value == 0 for value in u.data):
+    if all(value == 0 for value in v.data) or all(value == 0 for value in u.data): # Loop through v and u O(n)
         raise ValueError("A vector cannot be null.")
 
-    if u.get_size() != v.get_size():
+    if u.get_size() != v.get_size(): # Space and time complexity of O(1).
         raise ValueError("Vectors must have the same size.")
 
-    return round(u.dot(v) / (u.norm() * v.norm()), 8)
+    return round(u.dot(v) / (u.norm() * v.norm()), 9) # Operations using functions of complexity O(n) so time complexity of O(n)
